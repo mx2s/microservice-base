a6b1bdb72c78b9caf4857e59aaaea6ba93049e42
Max K <mxss1998@yandex.ru>
2018-07-27 08:19:34 +0300

    Debug module

---
+++ Scripts/Modules/Debug/DebugLog.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace SharpyJson.Scripts.Modules.Debug
+{
+    public class DebugLog
+    {
+        private LogLevels logLevel = LogLevels.None;
+        
+        private static DebugLog instance;
+        
+        private DebugLog() {}
+        
+        public static DebugLog get() {
+            if (instance == null) {
+                instance = new DebugLog();
+            }
+
+            return instance;
+        }
+        
+        public void SetLogLevel(LogLevels level) {
+            logLevel = level;
+        }
+        
+        public void Message(string message) {
+            if (logLevel <= LogLevels.Message) {
+                Console.WriteLine("Message: " + message);
+            }
+        }
+        
+        public void Log(string message) {
+            if (logLevel <= LogLevels.Log) {
+                Console.WriteLine("Log: " + message);
+            }
+        }
+        
+        public void Warning(string message) {
+            if (logLevel <= LogLevels.Warning) {
+                Console.WriteLine("Warning: " + message);
+            }
+        }
+        
+        public void Error(string message) {
+            if (logLevel <= LogLevels.Error) {
+                Console.WriteLine("Error: " + message);
+            }
+        }
+        
+        public void Fatal(string message) {
+            if (logLevel <= LogLevels.Fatal) {
+                Console.WriteLine("Fatal: " + message);
+            }
+        }
+    }
+}
---
+++ Scripts/Modules/Debug/LogLevels.cs
@@ -0,0 +1,13 @@
+﻿namespace SharpyJson.Scripts.Modules.Debug
+{
+    public enum LogLevels
+    {
+        None = 0,
+
+        Message = 1,
+        Log = 2,
+        Warning = 3,
+        Error = 4,
+        Fatal = 5
+    }
+}
